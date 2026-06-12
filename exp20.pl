613aa960535ec820fb030e431ffaff464334a3f1
SRI MATHI <192424095.simats@saveetha.com>
2026-06-12 08:36:12 +0530

    Add files via upload

---
+++ exp20.pl
@@ -0,0 +1,8 @@
+planet(mercury,57.9).
+planet(venus,108.2).
+planet(earth,149.6).
+planet(mars,227.9).
+planet(jupiter,778.5).
+planet(saturn,1433.5).
+planet(uranus,2872.5).
+planet(neptune,4495.1).
