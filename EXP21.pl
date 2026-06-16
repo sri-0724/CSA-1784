7d550f48469dfc7e3691faa78e5529b97536456a
SRI MATHI <192424095.simats@saveetha.com>
2026-06-16 13:05:57 +0530

    Add files via upload

---
+++ EXP21.pl
@@ -0,0 +1,12 @@
+hanoi(1,Source,Destination,_) :-
+    write('Move '),
+    write(Source),
+    write(' -> '),
+    write(Destination), nl.
+
+hanoi(N,Source,Destination,Auxiliary) :-
+    N > 1,
+    N1 is N-1,
+    hanoi(N1,Source,Auxiliary,Destination),
+    hanoi(1,Source,Destination,Auxiliary),
+    hanoi(N1,Auxiliary,Destination,Source).
